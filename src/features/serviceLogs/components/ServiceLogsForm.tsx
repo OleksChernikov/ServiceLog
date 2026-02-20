8422e9c0cc4303ba194af3eeb811d28287b1ec14
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2026-02-20 18:31:48 +0200

    fix Date icon

--- src/features/serviceLogs/components/ServiceLogsForm.tsx
+++ src/features/serviceLogs/components/ServiceLogsForm.tsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { type AppDispatch, type RootState } from '../../../app/store';
 import { createDraft, clearAllDrafts } from '../store/draftSlice';
 import { addServiceLog } from '../store/serviceLogsSlice';
+// import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const getToday = () => new Date().toISOString().split('T')[0];
 
@@ -103,151 +104,168 @@ export const ServiceLogForm = () => {
 
   return (
     <div className='form-container'>
-    <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ p: 2 }}>
-      <Typography variant="h5" mb={4} sx={{ color: '#1976d2' }}>
-        Create Service Log
-      </Typography>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ p: 2 }}>
+        <Typography variant="h5" mb={4} sx={{ color: '#1976d2' }}>
+          Create Service Log
+        </Typography>
 
-      <Grid container spacing={2}>
-        {[
-          { name: 'providerId', label: 'Provider ID' },
-          { name: 'serviceOrder', label: 'Service Order' },
-          { name: 'carId', label: 'Car ID' },
-        ].map((field) => (
-          <Grid size={{ xs:12, md:6 }} key={field.name}>
+        <Grid container spacing={2}>
+          {[
+            { name: 'providerId', label: 'Provider ID' },
+            { name: 'serviceOrder', label: 'Service Order' },
+            { name: 'carId', label: 'Car ID' },
+          ].map((field) => (
+            <Grid size={{ xs:12, md:6 }} key={field.name}>
+              <Controller
+                name={field.name as keyof ServiceLogFormValues}
+                control={control}
+                render={({ field: controllerField }) => (
+                  <TextField
+                    {...controllerField}
+                    fullWidth
+                    label={field.label}
+                    error={!!errors[field.name as keyof ServiceLogFormValues]}
+                    helperText={
+                      errors[field.name as keyof ServiceLogFormValues]?.message
+                    }
+                  />
+                )}
+              />
+            </Grid>
+          ))}
+
+          <Grid size={{ xs:12, md:6 }}>
             <Controller
-              name={field.name as keyof ServiceLogFormValues}
+              name="odometr"
               control={control}
-              render={({ field: controllerField }) => (
+              render={({ field }) => (
                 <TextField
-                  {...controllerField}
+                  {...field}
+                  type="number"
                   fullWidth
-                  label={field.label}
-                  error={!!errors[field.name as keyof ServiceLogFormValues]}
-                  helperText={
-                    errors[field.name as keyof ServiceLogFormValues]?.message
-                  }
+                  label="Odometer (mi)"
+                  error={!!errors.odometr}
+                  helperText={errors.odometr?.message}
+                  sx={{
+                    '& input[type=number]::-webkit-inner-spin-button': {
+                      filter: 'invert(2)',
+                    },
+                  }}
                 />
               )}
             />
           </Grid>
-        ))}
 
-        <Grid size={{ xs:12, md:6 }}>
-          <Controller
-            name="odometr"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                type="number"
-                fullWidth
-                label="Odometer (mi)"
-                error={!!errors.odometr}
-                helperText={errors.odometr?.message}
+          <Grid size={{ xs:12, md:6 }}>
+            <Controller
+              name="engineHours"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type="number"
+                  fullWidth
+                  label="Engine Hours"
+                  error={!!errors.engineHours}
+                  helperText={errors.engineHours?.message}
+                  sx={{
+                    '& input[type=number]::-webkit-inner-spin-button': {
+                      filter: 'invert(2)',
+                    },
+                  }}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs:12, md:6 }}>
+            <Controller
+              name="startDate"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type="date"
+                  fullWidth
+                  label="Start Date"
+                  InputLabelProps={{ shrink: true }}
+                  SelectProps={{
+                  sx: { textAlign: 'left' }
+                }}
                 sx={{
-                  '& input[type=number]::-webkit-inner-spin-button': {
-                    filter: 'invert(2)',
+                  '& input::-webkit-calendar-picker-indicator': {
+                    display: 'block', 
+                    cursor: 'pointer',
+                    filter: 'invert(0.5)', 
                   },
                 }}
-              />
-            )}
-          />
-        </Grid>
+                />
+              )}
+            />
+          </Grid>
 
-        <Grid size={{ xs:12, md:6 }}>
-          <Controller
-            name="engineHours"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                type="number"
-                fullWidth
-                label="Engine Hours"
-                error={!!errors.engineHours}
-                helperText={errors.engineHours?.message}
-                sx={{
-                  '& input[type=number]::-webkit-inner-spin-button': {
-                    filter: 'invert(2)',
+          <Grid size={{ xs:12, md:6 }}>
+            <Controller
+              name="endDate"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type="date"
+                  fullWidth
+                  label="End Date"
+                  InputLabelProps={{ shrink: true }}
+                  sx={{
+                  '& input::-webkit-calendar-picker-indicator': {
+                    display: 'block', 
+                    cursor: 'pointer',
+                    filter: 'invert(0.5)', 
                   },
                 }}
-              />
-            )}
-          />
-        </Grid>
+                />
+              )}
+            />
+          </Grid>
 
-        <Grid size={{ xs:12, md:6 }}>
-          <Controller
-            name="startDate"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                type="date"
-                fullWidth
-                label="Start Date"
-                InputLabelProps={{ shrink: true }}
-              />
-            )}
-          />
-        </Grid>
+          <Grid size={{ xs:12}}>
+            <Controller
+              name="type"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} select fullWidth label="Service Type">
+                  <MenuItem value="planned">Planned</MenuItem>
+                  <MenuItem value="unplanned">Unplanned</MenuItem>
+                  <MenuItem value="emergency">Emergency</MenuItem>
+                </TextField>
+              )}
+            />
+          </Grid>
 
-        <Grid size={{ xs:12, md:6 }}>
-          <Controller
-            name="endDate"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                type="date"
-                fullWidth
-                label="End Date"
-                InputLabelProps={{ shrink: true }}
-              />
-            )}
-          />
-        </Grid>
+          <Grid size={{ xs:12}}>
+            <Controller
+              name="serviceDescription"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  multiline
+                  rows={4}
+                  fullWidth
+                  label="Service Description"
+                  error={!!errors.serviceDescription}
+                  helperText={errors.serviceDescription?.message}
+                />
+              )}
+            />
+          </Grid>
 
-        <Grid size={{ xs:12}}>
-          <Controller
-            name="type"
-            control={control}
-            render={({ field }) => (
-              <TextField {...field} select fullWidth label="Service Type">
-                <MenuItem value="planned">Planned</MenuItem>
-                <MenuItem value="unplanned">Unplanned</MenuItem>
-                <MenuItem value="emergency">Emergency</MenuItem>
-              </TextField>
-            )}
-          />
+          <Grid size={{ xs:12}}>
+            <Button variant="contained" type="submit" color="primary">
+              Create Service Log
+            </Button>
+          </Grid>
         </Grid>
-
-        <Grid size={{ xs:12}}>
-          <Controller
-            name="serviceDescription"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                multiline
-                rows={4}
-                fullWidth
-                label="Service Description"
-                error={!!errors.serviceDescription}
-                helperText={errors.serviceDescription?.message}
-              />
-            )}
-          />
-        </Grid>
-
-        <Grid size={{ xs:12}}>
-          <Button variant="contained" type="submit" color="primary">
-            Create Service Log
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
     </div>
   );
 };
